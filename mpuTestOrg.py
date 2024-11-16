5c10229a7a015a2d40b754fa05f0a95cb2be85a2
akrenomeron <alyssakrenomeron@gmail.com>
2024-11-16 14:13:10 -0800

    new updates

--- mpuTestOrg.py
+++ mpuTestOrg.py
@@ -1,3 +1,5 @@
+#original program from library 
+
 import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
--- speedTest.py
+++ speedTest.py
@@ -1,3 +1,5 @@
+#tests speed
+
 import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
