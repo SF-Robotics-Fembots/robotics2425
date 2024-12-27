9c9d552869c1ca147057bf3094c9911206493b4a
kin tirumala <25tirumala@stfrancishs.org>
2024-12-27 13:36:05 -0800

    Update joyServer.py
    editing the joystick input code?

--- runfiles/joyServer.py
+++ runfiles/joyServer.py
@@ -1,72 +1,79 @@
+#libraries
 import pygame
-import socket
-import time, json
+import time
+import math
 
-#global ip_server
+#initialize the joystick?
+pygame.joystick.init()
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
+#initialize pygame
+pygame.init()
 
+#initializer variables
+init_ratio = 0
 
-def main(ip_server):
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(ip_server)
+def hatValue(ratio):
+    past_ratio = ratio
+    new_ratio = past_ratio
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+        if event.type == pygame.JOYHATMOTION:
+            #assign some values to the variables
+            #x - left (-1) right (1)
+            #y - up (1) down (-1)
+            x, y = pygame.joystick.Joystick(0).get_hat(0)
+            #either add or subtract x*0.1 to the ratio
+            new_ratio = new_ratio + x*0.1
 
-    #assign a ratio value
-    ratio = 0.6 #60% speed
-    
-    pygame.joystick.init()
-    joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-    
-    pygame.init()
-    
-    serverSocket.bind((ip_server, 9090)) #was 9090
-    serverSocket.listen(1)
-    print("socket listening joystick")
-    
-    (clientConnected, clientAddress) = serverSocket.accept()
-    
-    #the speed is initially set to fast
+            #the ratio may not exceed 1 or be less than 0
+            if new_ratio < 0:
+                new_ratio = 0
+            if new_ratio >= 1:
+                new_ratio = 1
+
+    #   if past_ratio != new_ratio:
+        return new_ratio
+  
+
+            
+
+def main():
     slow_speed = 0
+    initratio = 0
+    ratuo = 1
 
     while True:
         print("joystick loop")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 break
-            if event.type == pygame.JOYAXISMOTION:
-                print(event)
-            if event.type == pygame.JOYBUTTONDOWN:
-                print(event)
-            
+    
+    # collect joystick values as -1 to 1
 
         if pygame.joystick.Joystick(0).get_button(5): slow_speed = 0
         if pygame.joystick.Joystick(0).get_button(3): slow_speed = 1
+        
+        print("slow_speed: " + str(slow_speed))
 
         x_speed = (pygame.joystick.Joystick(0).get_axis(0))
-        if slow_speed: x_speed = x_speed*ratio
-        
-        y_speed = (pygame.joystick.Joystick(0).get_axis(1))
-        if slow_speed: y_speed = y_speed*ratio
-       
-        r_speed = (pygame.joystick.Joystick(0).get_axis(2))
-        if slow_speed: r_speed = r_speed*ratio
-       
-        v_speed = (pygame.joystick.Joystick(0).get_axis(3))
+
+        ratuo = hatValue(initratio)
+        print("ratuo: " + str(ratuo))
+        if slow_speed: x_speed = x_speed*ratuo
+
 
         #put the thruster values in the dictionary
         thrusterMovements = {
-            'x_speed' : x_speed,
-            'y_speed' : y_speed,
-            'r_speed' : r_speed,
-            'v_speed' : v_speed
+            'x_speed' : x_speed
         }
-        
-        #send the json dict
-        message = json.dumps(thrusterMovements)
-        message = message.encode()
-        clientConnected.send(message)
 
-        #to slow down the loop because it goes faster than light
-        time.sleep(.01)
+        print(thrusterMovements)
+        #reset the init_ratio
+        initratio = ratuo
+
         
 if __name__ == "__main__":
     main()
+
